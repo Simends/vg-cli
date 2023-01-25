d3437c7f545d9717bdeefec0488a708864f522e4
Simen Dager Sneve <smndagersneve@gmail.com>
2023-01-25 11:06:17 +0100

    revert pretty printing with tabular

--- vg-cli.py
+++ vg-cli.py
@@ -3,7 +3,6 @@ import sys
 import json
 import requests
 from bs4 import BeautifulSoup
-from tabulate import tabulate
 
 SCRAPE_INTERVAL = 10
 
@@ -31,11 +30,10 @@ def getPublicationDate(tracking_data):
         publication_date = tracking_data['changes']['firstPublished']
         return publication_date.replace("T", " ").replace("Z", "")
     except TypeError:
-        return ""
+        return "                   "
 
-def main(_):
+def main():
     articles = getArticles()
-    table = []
     for article in articles:
         tracking_data = getTrackingData(article)
         id = getId(tracking_data)
@@ -43,9 +41,8 @@ def main(_):
             continue
         publication_date = getPublicationDate(tracking_data)
         title = getTitle(tracking_data)
-        table.append([title, publication_date])
+        print(publication_date + " " + title)
         printed_articles.append(id)
-    print(tabulate(table, headers=["Tittel", "Publiserings tidspunkt"], tablefmt="simple"))
 
 if __name__ == "__main__":
     main(sys.argv[1:])
